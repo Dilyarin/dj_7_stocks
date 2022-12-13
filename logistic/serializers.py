100aee762ce6e1643f8084e2342bd5bf6e53ef3b
Dilyarin <dilyarin@gmail.com>
2022-12-13 20:12:11 +0300

    with changes update_or_create, search

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -9,48 +9,36 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    product = serializers.IntegerField()
-    # stock = serializers.IntegerField()
-    price = serializers.FloatField()
 
     class Meta:
         model = StockProduct
         fields = ['id', 'product', 'price', 'quantity']
 
 class StockSerializer(serializers.ModelSerializer):
-    address = serializers.CharField(max_length=100)
     positions = ProductPositionSerializer(many=True)
 
     class Meta:
         model = Stock
-        fields = ['id', 'address', 'positions']
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
         stock = super().create(validated_data)
 
         for d in positions:
             StockProduct.objects.create(stock=stock, **d)
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
 
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
         for d in positions:
-            StockProduct.objects.update(stock=stock, **d)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=d['product'],
+                defaults={'price': d['price'], 'quantity': d['quantity']})
 
         return stock
 
